f3d12c504ff6ab4cd4b98c237586f845c3b203db
Rios-1234 <rh10@kent.ac.uk>
2026-03-06 11:18:37 +0000

    Implement PasswordChecker class in checker.py
    Add PasswordChecker class to check password validity and manage history.

--- checker.py
+++ checker.py
@@ -1,1 +1,12 @@
+import re
+from password import Password
 
+class PasswordChecker:
+    def __init__(self, history_file="history.txt"):
+        # Store the filename where password history will be written.
+        # Keeping this as an attribute allows the user to change the file if needed.
+        self.history_file = history_file
+
+    def check(self, password: Password) -> dict:
+        # Get the raw text of the password from the Password object.
+        raw = password.raw

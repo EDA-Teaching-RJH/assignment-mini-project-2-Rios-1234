86fec21b9c2283e6d7795cb1126e9fde874ccac3
Rios-1234 <rh10@kent.ac.uk>
2026-03-10 12:02:01 +0000

    Implement PasswordChecker class
    Adding PasswordChecker class with history file initialization

--- checker.py
+++ checker.py
@@ -1,1 +1,6 @@
 
+
+class PasswordChecker:
+    def __init__(self, history_file="history.csv"):
+        # Store the filename where password history will be saved
+        self.history_file = history_file

e5ba3b2d3889e15f23e748f9d3c4a3f998491bfe
Rios-1234 <rh10@kent.ac.uk>
2026-03-10 13:13:30 +0000

    Initialize password history CSV file i

--- checker.py
+++ checker.py
@@ -4,3 +4,12 @@ class PasswordChecker:
     def __init__(self, history_file="history.csv"):
         # Store the filename where password history will be saved
         self.history_file = history_file
+          # Create the CSV file with headers if it does not already exist.
+        # Using mode "x" means: create file, but raise error if it exists.
+        try:
+            with open(self.history_file, "x", newline="") as f:
+                writer = csv.writer(f)
+                writer.writerow(["password", "score", "label"])
+        except FileExistsError:
+            # File already exists — nothing to do
+            pass

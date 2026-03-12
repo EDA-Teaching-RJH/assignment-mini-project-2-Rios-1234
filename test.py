4256839f8fa4e24573cd3dc8b12260c32e11f47c
Rios-1234 <rh10@kent.ac.uk>
2026-03-12 08:52:41 +0000

    Delete test_cli_check function
    Removed test for CLI password check functionality.

--- test.py
+++ test.py
@@ -1,11 +1,2 @@
 
-def test_cli_check(capsys):
-    # Simulate: python main.py check Aa1!aaaa
-    sys.argv = ["main.py", "check", "Aa1!aaaa"]
 
-    main.main()
-
-    output = capsys.readouterr().out
-
-    # Should print the cowsay message for check results
-    assert "Password check results!" in output

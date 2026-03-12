0e82b21a26a4722a3c4a45e2ebb7900952d1c6e3
Rios-1234 <rh10@kent.ac.uk>
2026-03-12 08:52:04 +0000

    Implement test for CLI password check
    Add a test for the CLI check functionality.

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
 
+def test_cli_check(capsys):
+    # Simulate: python main.py check Aa1!aaaa
+    sys.argv = ["main.py", "check", "Aa1!aaaa"]
+
+    main.main()
+
+    output = capsys.readouterr().out
+
+    # Should print the cowsay message for check results
+    assert "Password check results!" in output
